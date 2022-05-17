4f68417148b60d725036c4669b2afa6a4fbceda1
Rodrigo Gilabert <rgilabert07@gmail.com>
2022-05-17 14:43:38 -0300

    17/05

--- 03-bases-node/app.js
+++ 03-bases-node/app.js
@@ -1,29 +1,9 @@
 const { crearArchivo } = require("./helpers/multiplicar");
-const argv = require("yargs")
-  .option("b", {
-    alias: "base",
-    type: "number",
-    demandOption: true,
-    description: "Numero base de multiplicar",
-  })
-  .option("l", {
-    alias: "listar",
-    type: "boolean",
-    description: "Listar la tabla",
-    default: false,
-    demandOption: true,
-  })
-  .check((argv, options) => {
-    if (isNaN(argv.b)) {
-      throw "La base tiene que ser un numero";
-    }
-    return true;
-  }).argv;
+const argv = require("./config/yargs");
+require("colors");
 
 console.clear();
 
-console.log(argv);
-
-crearArchivo(argv.b, argv.l)
-  .then((nombreArchivo) => console.log(nombreArchivo, "creado"))
+crearArchivo(argv.b, argv.l, argv.h)
+  .then((nombreArchivo) => console.log(nombreArchivo.rainbow, "creado"))
   .catch((err) => console.log(err));
--- 03-bases-node/helpers/multiplicar.js
+++ 03-bases-node/helpers/multiplicar.js
@@ -1,20 +1,23 @@
 const fs = require("fs");
+const colors = require("colors");
 
-const crearArchivo = async (base = 5, listar = false) => {
+const crearArchivo = async (base = 5, listar = false, hasta = 10) => {
   try {
     let salida = "";
-    for (let i = 1; i <= 10; i++) {
+    let consola = "";
+    for (let i = 1; i <= hasta; i++) {
       salida += `${base} x ${i} = ${base * i} \n`;
+      consola += `${base} ${"x".green} ${i} ${"=".green} ${base * i} \n`;
     }
 
     if (listar) {
-      console.log("====================");
-      console.log("    Tabla del:", base);
-      console.log("====================");
-      console.log(salida);
+      console.log("====================".green);
+      console.log("    Tabla del:".green, colors.blue(base));
+      console.log("====================".green);
+      console.log(consola);
     }
 
-    fs.writeFileSync(`tabla-${base}.txt`, salida);
+    fs.writeFileSync(`./salida/tabla-${base}.txt`, salida);
     return `tabla-${base}.txt`;
   } catch (error) {
     throw error;
