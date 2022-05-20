8d34cf28026e19c6b25e31dfa758a2867f5a8110
Rodrigo Gilabert <rgilabert07@gmail.com>
2022-05-20 17:15:34 -0300

    20/05

---
+++ 04-lista-tareas/app.js
@@ -0,0 +1,27 @@
+require("colors");
+const { inquirerMenu, pausa, leerInput } = require("./helpers/inquirer");
+const Tareas = require("./models/tareas");
+
+const main = async () => {
+  let opt = "";
+  const tareas = new Tareas();
+  do {
+    opt = await inquirerMenu();
+
+    switch (opt) {
+      case "1":
+        // crear
+        const desc = await leerInput("Descripcion:");
+        tareas.crearTarea(desc);
+        break;
+      case "2":
+        console.log(tareas._listado);
+        break;
+      default:
+        break;
+    }
+
+    await pausa();
+  } while (opt !== "0");
+};
+main();
