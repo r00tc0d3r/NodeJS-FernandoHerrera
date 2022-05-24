e931e7dee0c82e9a671b4ece7cb5856c13add3fb
Rodrigo Gilabert <rgilabert07@gmail.com>
2022-05-24 15:43:04 -0300

    Seccion 5 Terminada 24/05

--- 04-lista-tareas/app.js
+++ 04-lista-tareas/app.js
@@ -1,10 +1,25 @@
 require("colors");
-const { inquirerMenu, pausa, leerInput } = require("./helpers/inquirer");
+const { guardarDB, leerDB } = require("./helpers/guardarArchivo");
+const {
+  inquirerMenu,
+  pausa,
+  leerInput,
+  listadoTareasBorrar,
+  confirmar,
+  mostrarListadoChecklist,
+} = require("./helpers/inquirer");
 const Tareas = require("./models/tareas");
 
 const main = async () => {
   let opt = "";
   const tareas = new Tareas();
+
+  const tareasDB = leerDB();
+  if (tareasDB) {
+    // establecer tareas
+    tareas.cargarTareaFromArray(tareasDB);
+  }
+
   do {
     opt = await inquirerMenu();
 
@@ -14,12 +29,32 @@ const main = async () => {
         const desc = await leerInput("Descripcion:");
         tareas.crearTarea(desc);
         break;
-      case "2":
-        console.log(tareas._listado);
+      case "2": // listado completo
+        tareas.listadoCompleto();
         break;
-      default:
+      case "3": // listar completadas
+        tareas.listarPendientesCompletadas(true);
+        break;
+      case "4": // listar pendientes
+        tareas.listarPendientesCompletadas(false);
+        break;
+      case "5": // completado | pendiente
+        const ids = await mostrarListadoChecklist(tareas.listadoArr);
+        tareas.toggleCompletadas(ids);
+        break;
+      case "6": // borrar
+        const id = await listadoTareasBorrar(tareas.listadoArr);
+        if (id !== "0") {
+          const ok = await confirmar("¿Está seguro?");
+          if (ok) {
+            tareas.borrarTarea(id);
+            console.log("Tarea Borrada");
+          }
+        }
         break;
     }
+
+    guardarDB(tareas.listadoArr);
 
     await pausa();
   } while (opt !== "0");
---
+++ 04-lista-tareas/helpers/guardarArchivo.js
@@ -0,0 +1,21 @@
+const fs = require("fs");
+const { CLIENT_RENEG_LIMIT } = require("tls");
+
+const archivo = "./db/data.json";
+
+const guardarDB = (data) => {
+  fs.writeFileSync(archivo, JSON.stringify(data));
+};
+
+const leerDB = () => {
+  if (!fs.existsSync(archivo)) {
+    return null;
+  }
+
+  const info = fs.readFileSync(archivo, { encoding: "utf8" });
+  const data = JSON.parse(info);
+
+  return data;
+};
+
+module.exports = { guardarDB, leerDB };
